ef4eaeafc9fb4dcaf293d3af38da4d8d7e64117d
Magnolist Mint <ashe.moira@gmail.com>
2018-09-18 04:03:16 +0900

    feat: change test to rspec and gemerate book template

---
+++ spec/views/books/new.html.erb_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'books/new', type: :view do
+  before(:each) do
+    assign(:book, Book.new(
+                    title: 'MyString',
+                    author: 'MyString',
+                    genre: 1,
+                    category: 1,
+                    page: 1
+                  ))
+  end
+
+  it 'renders new book form' do
+    render
+
+    assert_select 'form[action=?][method=?]', books_path, 'post' do
+      assert_select 'input[name=?]', 'book[title]'
+
+      assert_select 'input[name=?]', 'book[author]'
+
+      assert_select 'input[name=?]', 'book[genre]'
+
+      assert_select 'input[name=?]', 'book[category]'
+
+      assert_select 'input[name=?]', 'book[page]'
+    end
+  end
+end
