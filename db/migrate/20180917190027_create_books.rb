af17ea45ea214fd54c0330992e3535d115200a10
Magnolist Mint <ashe.moira@gmail.com>
2018-09-21 01:17:49 +0900

    feat: create book table

--- db/migrate/20180917190027_create_books.rb
+++ db/migrate/20180917190027_create_books.rb
@@ -3,8 +3,8 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.string :title
-      t.string :author
+      t.string :title, limit: 192, null: false, default: ''
+      t.string :author, limit: 192, null: false, default: ''
       t.integer :genre
       t.integer :category
       t.date :publication_date
