612826c38d315a1b6d4a22179807413efea9b45f
Magnolist Mint <ashe.moira@gmail.com>
2018-09-25 02:24:57 +0900

    feat: create book_spec, but it is no complete

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Book < ApplicationRecord
+  validates :title, length: { maximum: 192 }, presence: true
+  validates :author, length: { maximum: 192 }, presence: true
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,5 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:book) { Book.new }
+
+  describe 'title validate checking' do
+    it 'it is invalid to 193 or more characters' do
+      book.title = '0' * 193
+      book.author = '0'
+      expect(book).to be_invalid
+    end
+
+    it 'it is valid to 192 characters or less' do
+      book.title = '0' * 192
+      book.author = '0'
+      expect(book).to be_valid
+    end
+  end
 end
