ed997570d47f3df8aec1f24d1c4e41bdb98a7e27
Magnolist Mint <ashe.moira@gmail.com>
2018-09-27 03:23:58 +0900

    feat: add #index test

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -3,10 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Books', type: :request do
-  describe 'GET /books' do
-    it 'works! (now write some real specs)' do
-      get books_path
-      expect(response).to have_http_status(200)
+  let(:book) { FactoryBot.create(:book) }
+
+  def before_create_book
+    book
+  end
+
+  describe 'GET #index' do
+    it 'returns a success response' do
+      get books_url, params: {}
+      expect(response.status).to eq 200
+    end
+
+    it 'include book title and author' do
+      before_create_book
+      get books_url, params: {}
+      expect(response.body).to include book.title
     end
   end
 end
